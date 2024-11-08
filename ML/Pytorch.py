782fc6be4e71760dd28710307b6bde149bd1e51d
hadar88 <hadargantz@gmail.com>
2024-11-08 17:03:18 +0200

    changes

--- ML/Pytorch.py
+++ ML/Pytorch.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 X = torch.tensor([1, 2, 3, 4], dtype = torch.float32)
-Y = torch.tensor([7, 12, 17, 22], dtype = torch.float32)
+Y = torch.tensor([16.8, 29.3, 41.8, 54.3], dtype = torch.float32)
 
 w = torch.tensor(0.0, dtype = torch.float32, requires_grad=True)
 b = torch.tensor(0.0, dtype = torch.float32, requires_grad=True)
@@ -13,7 +13,7 @@ def forward(x):
 loss = nn.MSELoss()
 opt = torch.optim.SGD([w, b], lr=0.01)
 
-n_iter = 1000
+n_iter = 100000
 
 for i in range(n_iter):
     y_pred = forward(X)
@@ -26,6 +26,7 @@ for i in range(n_iter):
 
     opt.zero_grad()
 
-    if(i == 999):
-        print(f'w = {w}, b = {b}, loss = {l}, y_pred = {y_pred}, dw = {w.grad}, f10 = {forward(10)}')
+    if(l < 0.00000001):
+        print(f'w = {w:.3f}, b = {b:.3f}, loss = {l:.8f}, y_pred = {y_pred}, f10 = {forward(10):.3f}')
+        break
 
