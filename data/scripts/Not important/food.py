225d5b52a0ced7a605d39ee0a449d95b3589999c
hadar88 <hadargantz@gmail.com>
2024-12-19 16:21:33 +0200

    changes

--- data/scripts/Not important/food.py
+++ data/scripts/Not important/food.py
@@ -1,44 +1,23 @@
 import json
 
-with open('FoodDataFirst.json', 'r') as file:
+with open('../../initial_data/FoodDataFirst.json', 'r') as file:
     data = json.load(file)
 
 nutrients_to_keep = [
-    "Protein",
-    "Fat",
-    "Carbohydrate",
-    "Calories",
-    "Sugars", 
-    "Fiber",
-    "Water",
-    "Cholesterol",
-    "Saturated fat",
-    "Calcium",
-    "Iron",
-    "Magnesium",
-    "Vitamin A",
-    "Vitamin B-12",
-    "Vitamin C",
-    "Vitamin D",
-    "Vitamin E",
-    "Vitamin K",
+    "Sodium"
 ]
 
+filtered_food = {}
 for food in data.get('Foods', []):
-    filtered_food = {
-        'Food Name': food.get('Food Name', ''),
-        'Nutritional data': []
-    }
     for nutrient in food.get('Nutritional data', []):
         nutrient_name = nutrient.get('nutrient', {}).get('name')
         if nutrient_name in nutrients_to_keep:
-            filtered_food['Nutritional data'].append({
-                'name': nutrient_name,
-                'amount': nutrient.get('amount')
-            })
-    food.clear()
-    food.update(filtered_food)
+            filtered_food[food.get('Food Name', '')]= nutrient.get('amount')
+
+data = filtered_food
 
 
-with open('FoodDataFinal.json', 'w') as file:
+
+
+with open('FoodDataSodium.json', 'w') as file:
     json.dump(data, file, indent=4)
