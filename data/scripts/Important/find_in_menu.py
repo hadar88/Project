9ce4cb9d99c5220e50d07d782e61d1553b62a4d5
hadar88 <hadargantz@gmail.com>
2025-01-02 11:56:24 +0200

    changes

---
+++ data/scripts/Important/find_in_menu.py
@@ -0,0 +1,28 @@
+import json
+
+## input ##
+
+menu_id = input("Id: ")
+check = input("Test: ")
+
+###########
+
+foodbyid = open("../../layouts/FoodsByID.json", "r")
+data = json.load(foodbyid)
+foodbyid.close()
+
+menus = open("../../layouts/menus.json", "r")
+menus_data = json.load(menus)
+menus.close()
+
+menu = menus_data[menu_id]
+
+for day in menu:
+    for meal in menu[day]:
+        for id in menu[day][meal]:
+            food = data[id]
+            if food[check] == 1:
+                print(id + " - " + food["Name"])
+
+
+
