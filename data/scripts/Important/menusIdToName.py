0b14efe10fa3099fae477a2e0469944bbd8333bb
hadar88 <hadargantz@gmail.com>
2025-01-11 20:07:41 +0200

    changes

--- data/scripts/Important/menusIdToName.py
+++ data/scripts/Important/menusIdToName.py
@@ -29,13 +29,35 @@ for menuid in menusById:
                     menusName[menuid][day][meal] = {}
 
                 menusName[menuid][day][meal][foodname] = amount
-                
-                
-
 
 menusByName = open("../../layouts/menusByName.json", "w")
 json.dump(menusName, menusByName, indent=4)
 menusByName.close()
 
+
+
+
+db = open("../../layouts/menusByName.json")
+dict = json.load(db)
+db.close()
+
+
+onelinedb = open("../../layouts/menusByName.json", "w")
+
+onelinedb.writelines("{\n")
+
+for id in dict:
+    onelinedb.writelines('\t"' + id + '": {\n')
+    for day in dict[id]:
+        onelinedb.writelines('\t\t"' + day + '": ')
+        onelinedb.writelines(json.dumps(dict[id][day]))
+        if day != "saturday":
+            onelinedb.writelines(",\n")
+        else:
+            onelinedb.writelines("\n")
+    onelinedb.writelines("\t},\n")
+
+onelinedb.writelines("}")
+onelinedb.close()
 
 
