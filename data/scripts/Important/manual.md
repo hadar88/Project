d66dc1e2cc1c08ce7b64e91ed467afced0434d92
hadar88 <hadargantz@gmail.com>
2025-01-11 14:02:59 +0200

    changes

--- data/scripts/Important/manual.md
+++ data/scripts/Important/manual.md
@@ -1,23 +1,36 @@
 # `add_nutritional_data.py`
  
 This script adds nutritional data to the `FoodsByName.json` and `FoodsByID.json` files.
+[View Script](./add_nutritional_data.py)
 
 # `algorithm.py` 
-This script calculates the nututionals values based on the user input
 
+This script calculates the nututionals values based on the user input
+[View Script](./algorithm.py)
 
 # `find_in_menu.py` 
 
 This script finds the foods in a certain menu that are vegeterian, vegan or contain some kind of allergan
+[View Script](./find_in_menu.py)
 
 # `menu_calculator.py`
 
-This script calculates the nutritional data of a menu and puts it in the file `MenusInput.json` 
+This script calculates the nutritional data of a menu and puts it in the file `MenusInput.json`  
+[View Script](./menu_calculator.py)
+
+Use this to make the menu in the correct format: `do all the inside keys, strings`
 
 # `move_menu_data.py`
 
 This script moves data in the file `MenusInput.json` from the `Menu` section to the `Initial` section
+[View Script](./move_menu_data.py)
 
 # `oneline.py`
 
 This script build a json file which contains foods and their id's and put it in the file `FoodByNameOneLine.py`
+[View Script](./oneline.py)
+
+# `menusIdToName.py`
+
+This script reates a dictionary with the same structure as `menusById`, but with the food names instead of the food ids
+[View Script](./menusIdToName.py)
--- data/scripts/Important/menusIdToName.py
+++ data/scripts/Important/menusIdToName.py
@@ -1,28 +1,43 @@
 import json
 
-menus = open("../../layouts/menusId.json", "r")
-menusId = json.load(menus)
+
+menus = open("../../layouts/menusById.json", "r")
+menusById = json.load(menus)
+menusName = {}
 menus.close()
 
 foods = open("../../layouts/FoodsByID.json", "r")
-foodsbyid = json.load(foods)
+foodsByid = json.load(foods)
 foods.close()
 
-menusName = open("../../layouts/menusName.json", "w")
 
-for menuid in menusId:
-    for day in menusId[menuid]:
-        for meal in menusId[menuid][day]:
-            for foodid in menusId[menuid][day][meal]:
-                if foodid not in foodsbyid:
+# Create a dictionary with the same structure as menusById, but with the food names instead of the food ids
+
+for menuid in menusById:
+    for day in menusById[menuid]:
+        for meal in menusById[menuid][day]:
+            for foodid in menusById[menuid][day][meal]:
+                if foodid not in foodsByid:
                     print("Food not found: " + foodid)
                     continue
-                foodname = foodsbyid[foodid]["Name"]
-                menusId[menuid][day][meal][foodid] = foodname
+                foodname = foodsByid[foodid]["Name"]
+                amount = menusById[menuid][day][meal][foodid]
+
+                if menuid not in menusName:
+                    menusName[menuid] = {}
+                if day not in menusName[menuid]:
+                    menusName[menuid][day] = {}
+                if meal not in menusName[menuid][day]:
+                    menusName[menuid][day][meal] = {}
+
+                menusName[menuid][day][meal][foodname] = amount
+                
+                
 
 
-json.dump(menusId, menusName, indent=4)
-menusName.close()
+menusByName = open("../../layouts/menusName.json", "w")
+json.dump(menusName, menusByName, indent=4)
+menusByName.close()
 
 
 
