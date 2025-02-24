c8d89fa7dae444ba090d20a99a5e09a2c7817844
hadar88 <hadargantz@gmail.com>
2025-02-24 17:12:02 +0200

    changes

--- data/scripts/Important/build.py
+++ data/scripts/Important/build.py
@@ -50,6 +50,6 @@ def generate_data(start, end):
     with open("templates.json", "w") as f:
         json.dump(data, f, indent=4)
 
-generate_data(1845, 2000)
+generate_data(2001, 2103)
 
 print("Templates generated successfully!")
--- data/scripts/Important/menu_calculator.py
+++ data/scripts/Important/menu_calculator.py
@@ -10,8 +10,8 @@ menus.close()
 
 ## fiil the data
 
-begin = 1990
-end = 2000
+begin = 2001
+end = 2103
 
 for menu_id in range(begin, end + 1):
     days = menudata[str(menu_id)]
