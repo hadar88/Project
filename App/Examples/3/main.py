9cf5f2e85f6da101745585fec028968efca43a1f
hadar88 <hadargantz@gmail.com>
2025-03-04 19:57:33 +0200

    change

--- App/Examples/3/main.py
+++ App/Examples/3/main.py
@@ -1,2 +1,11 @@
 import kivy
+from kivy.app import App
+from  kivy.uix.button import Button
 
+class MainApp(App):
+    def build(self):
+        return Button(text="Hello World")
+    
+
+if __name__ == "__main__":
+    MainApp().run()
