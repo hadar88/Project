47642254e27a958ef82944b218536e4ba1d88844
gmoalemm <moalemm@post.bgu.ac.il>
2025-03-06 01:16:06 +0200

    discarded chebyshev for good, replaced it with bell curves (it's much faster!). shortened the code significantly.

--- Model/The_Model/model.py
+++ Model/The_Model/model.py
@@ -7,12 +7,11 @@ import torch.optim as optim
 import torch.nn.functional as F
 from tqdm import tqdm
 import matplotlib
-matplotlib.use('TkAgg')
+
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-import numpy as np
 from make_dataset import read_foods_tensor, FoodProperties as FP
 import torch
-from numpy.polynomial.chebyshev import Chebyshev
 
 
 BATCH_SIZE = 256
@@ -28,6 +27,7 @@ if __name__ == "__main__":
 # print("Loading Testset...")
 # test_set = MenusDataset(train=False)
 # test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=False)
+
 
 class MenuGenerator(nn.Module):
     def __init__(self):
@@ -45,174 +45,85 @@ class MenuGenerator(nn.Module):
         y = torch.relu(self.fc3(y))
         y = y.reshape(-1, 7, 3, 10, 2)
 
-        # y = torch.round(y)
-        
         return y
 
+
 ###### Loss ##########
+
 
 class MenuLoss(nn.Module):
     def __init__(self, device):
         super(MenuLoss, self).__init__()
         self.ZERO_PENALTY = 2500.0
-        self.HIGHEST_ID = 222
         self.device = device
 
-        data = read_foods_tensor()
-        x = torch.tensor(range(len(data)))
-        
-        # Compute Chebyshev coefficients
-        calories = data[:, FP.CALORIES.value]
-        calories_poly = Chebyshev.fit(x, calories, 446)
-        self.calories_coeffs = torch.tensor(calories_poly.coef).to(device)
+        self.data = read_foods_tensor().to(device)
 
-        carbohydrate = data[:, FP.CARBOHYDRATE.value]
-        carbohydrate_poly = Chebyshev.fit(x, carbohydrate, 446)
-        self.carbohydrate_coeffs = torch.tensor(carbohydrate_poly.coef).to(device)
+    def get_binary_value(self, x, category: FP):
+        return torch.sum(
+            torch.stack([
+                    v * torch.exp(-((x - i * v) ** 2) / 0.01)
+                    for i, v in enumerate(self.data[:, category.value])],
+                dim=0,
+            ),
+            dim=0,
+        )
 
-        sugars = data[:, FP.SUGARS.value]
-        sugars_poly = Chebyshev.fit(x, sugars, 446)
-        self.sugars_coeffs = torch.tensor(sugars_poly.coef).to(device)
-
-        fat = data[:, FP.FAT.value]
-        fat_poly = Chebyshev.fit(x, fat, 446)
-        self.fat_coeffs = torch.tensor(fat_poly.coef).to(device)
-
-        proteins = data[:, FP.PROTEIN.value]
-        proteins_poly = Chebyshev.fit(x, proteins, 446)
-        self.proteins_coeffs = torch.tensor(proteins_poly.coef).to(device)
-
-        self.vegetarian = data[:, FP.VEGETARIAN.value]
-        self.vegan = data[:, FP.VEGAN.value]
-        self.contains_eggs = data[:, FP.CONTAINS_EGGS.value]
-        self.contains_gluten = data[:, FP.CONTAINS_GLUTEN.value]
-        self.contains_milk = data[:, FP.CONTAINS_MILK.value]
-        self.contains_peanuts = data[:, FP.CONTAINS_PEANUTS_OR_NUTS.value]
-        self.contains_fish = data[:, FP.CONTAINS_FISH.value]
-        self.contains_soy = data[:, FP.CONTAINS_SOY.value]
-        self.contains_sesame = data[:, FP.CONTAINS_SESAME.value]
-
-        self.fruits = data[:, FP.FRUIT.value]
-        self.vegetables = data[:, FP.VEGETABLE.value]
-        self.cheese = data[:, FP.CHEESE.value]
-        self.meat = data[:, FP.MEAT.value]
-        self.cereal = data[:, FP.CEREAL.value]
-
-    def is_vegetarian(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.vegetarian)], dim=0), dim=0)
-    
-    def is_vegan(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.vegan)], dim=0), dim=0)
-    
-    def is_contains_eggs(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.contains_eggs)], dim=0), dim=0)
-    
-    def is_contains_gluten(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.contains_gluten)], dim=0), dim=0)
-    
-    def is_contains_milk(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.contains_milk)], dim=0), dim=0)
-    
-    def is_contains_peanuts(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.contains_peanuts)], dim=0), dim=0)
-    
-    def is_contains_fish(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.contains_fish)], dim=0), dim=0)
-    
-    def is_contains_soy(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.contains_soy)], dim=0), dim=0)
-    
-    def is_contains_sesame(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.contains_sesame)], dim=0), dim=0)
-    
-    def is_fruits(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.fruits)], dim=0), dim=0)
-    
-    def is_vegetables(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.vegetables)], dim=0), dim=0)
-    
-    def is_cheese(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.cheese)], dim=0), dim=0)
-    
-    def is_meat(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.meat)], dim=0), dim=0)
-    
-    def is_cereal(self, x):
-        return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(self.cereal)], dim=0), dim=0)
+    def get_continuous_value(self, x, category: FP):
+        return torch.sum(
+            torch.stack(
+                [
+                    v * torch.exp(-((x - i) ** 2) / 0.01)
+                    for i, v in enumerate(self.data[:, category.value])
+                ],
+                dim=0,
+            ),
+            dim=0,
+        )
 
     def forward(self, y_pred, y):
-        pred_ids = y_pred[..., 0]       # Shape: (batch_size, 7, 3, M)
-        pred_amounts = y_pred[..., 1]   
+        pred_ids = y_pred[..., 0]  # Shape: (batch_size, 7, 3, M)
+        pred_amounts = y_pred[..., 1]
 
-        true_ids = y[..., 0]            
-        true_amounts = y[..., 1]        
+        true_ids = y[..., 0]
+        true_amounts = y[..., 1]
 
         ### Punish the model if it gives a positive amount for a 0-id food, or vice versa ###
 
-        case1 = 1 - torch.tanh(4 * pred_ids)         # ids == 0
+        case1 = 1 - torch.tanh(4 * pred_ids)  # ids == 0
         case2 = 1 - torch.tanh(4 * pred_amounts)  # amounts == 0
-        
-        zeros_nonzeros_penalty = self.ZERO_PENALTY * case1 * 2 + self.ZERO_PENALTY * case2       # Combine both cases
-        zeros_nonzeros_penalty = zeros_nonzeros_penalty.sum(dim=(1, 2, 3)).mean()       # Sum and average for all menus in the batch
+
+        zeros_nonzeros_penalty = self.ZERO_PENALTY * (2 * case1 + case2)  # Combine both cases
+        zeros_nonzeros_penalty = zeros_nonzeros_penalty.sum(dim=(1, 2, 3)).mean()  # Sum and average for all menus in the batch
 
         ### Penalize the model for predicting an id that is not in the dataset ###
-        
-        id_range_penalty = F.relu(pred_ids - self.HIGHEST_ID)
+
+        id_range_penalty = F.relu(pred_ids - 222)
 
         id_range_penalty = id_range_penalty.sum(dim=(1, 2, 3)).mean()
 
         ### Compute differences in calories, carbs, sugars, fat and proteins ###
 
         # True:
-        
-        true_calories = self.chebyshev_eval(true_ids, self.calories_coeffs)  # calories of each food in y (the true "label")
-        calories_in_true_menu = (true_calories * true_amounts / 100).sum(dim=(1, 2, 3)) / 7
 
-        true_carbohydrate = self.chebyshev_eval(true_ids, self.carbohydrate_coeffs)  # calories of each food in y (the true "label")
-        carbohydrate_in_true_menu = (true_carbohydrate * true_amounts / 100).sum(dim=(1, 2, 3)) / 7 
-        
-        true_sugars = self.chebyshev_eval(true_ids, self.sugars_coeffs)  # calories of each food in y (the true "label")
-        sugars_in_true_menu = (true_sugars * true_amounts / 100).sum(dim=(1, 2, 3)) / 7
-        
-        true_fat = self.chebyshev_eval(true_ids, self.fat_coeffs)  # calories of each food in y (the true "label")
-        fat_in_true_menu = (true_fat * true_amounts / 100).sum(dim=(1, 2, 3)) / 7
-        
-        true_proteins = self.chebyshev_eval(true_ids, self.proteins_coeffs)  # calories of each food in y (the true "label")
-        proteins_in_true_menu = (true_proteins * true_amounts / 100).sum(dim=(1, 2, 3)) / 7
+        nutrition_diff = 0.0
 
-        # Pred:
+        for fp in [FP.CALORIES, FP.CARBOHYDRATE]:
+            gold = (self.get_continuous_value(true_ids, fp) * true_amounts / 100).sum(dim=(1, 2, 3)) / 7
+            pred = (self.get_continuous_value(self.round_and_mask(pred_ids), fp) * pred_amounts / 100).sum(dim=(1, 2, 3)) / 7
+            nutrition_diff += (((gold - pred) / 100) ** 2).mean()
 
-        pred_calories = self.chebyshev_eval(self.round_and_mask(pred_ids), self.calories_coeffs)
-        calories_in_pred_menu = (pred_calories * pred_amounts / 100).sum(dim=(1, 2, 3)) / 7
-        
-        pred_carbohydrate = self.chebyshev_eval(self.round_and_mask(pred_ids), self.carbohydrate_coeffs)
-        carbohydrate_in_pred_menu = (pred_carbohydrate * pred_amounts / 100).sum(dim=(1, 2, 3)) / 7
-        
-        pred_sugars = self.chebyshev_eval(self.round_and_mask(pred_ids), self.sugars_coeffs)
-        sugars_in_pred_menu = (pred_sugars * pred_amounts / 100).sum(dim=(1, 2, 3)) / 7
-        
-        pred_fat = self.chebyshev_eval(self.round_and_mask(pred_ids), self.fat_coeffs)
-        fat_in_pred_menu = (pred_fat * pred_amounts / 100).sum(dim=(1, 2, 3)) / 7
-        
-        pred_proteins = self.chebyshev_eval(self.round_and_mask(pred_ids), self.proteins_coeffs)
-        proteins_in_pred_menu = (pred_proteins * pred_amounts / 100).sum(dim=(1, 2, 3)) / 7
-
-        # Diff:
-
-        calories_diff = (((calories_in_true_menu - calories_in_pred_menu)/100) ** 2).mean()
-        carbohydrate_diff = (((carbohydrate_in_true_menu - carbohydrate_in_pred_menu)/100) ** 2).mean()
-        sugars_diff = ((sugars_in_true_menu - sugars_in_pred_menu) ** 2).mean()
-        fat_diff = ((fat_in_true_menu - fat_in_pred_menu) ** 2).mean()
-        proteins_diff = ((proteins_in_true_menu - proteins_in_pred_menu) ** 2).mean()
-        
-        nutrition_diff = calories_diff + carbohydrate_diff + sugars_diff + fat_diff + proteins_diff
+        for fp in [FP.SUGARS, FP.FAT, FP.PROTEIN]:
+            gold = (self.get_continuous_value(true_ids, fp) * true_amounts / 100).sum(dim=(1, 2, 3)) / 7
+            pred = (self.get_continuous_value(self.round_and_mask(pred_ids), fp) * pred_amounts / 100).sum(dim=(1, 2, 3)) / 7
+            nutrition_diff += ((gold - pred) ** 2).mean()   # TODO: should we divide by 100?
 
         ### Compute the total loss ###
 
         loss = zeros_nonzeros_penalty + id_range_penalty + nutrition_diff
 
         return loss
-    
+
     class RoundSTE(torch.autograd.Function):
         @staticmethod
         def forward(ctx, input):
@@ -221,38 +132,19 @@ class MenuLoss(nn.Module):
         @staticmethod
         def backward(ctx, grad_output):
             return grad_output
-    
+
     def round_ste(self, input):
         return self.RoundSTE.apply(input)
 
     def mask(self, x):
-        """ approx. 0 for any id > 222 and the id itself for any id <= 222. """
+        """approx. 0 for any id > 222 and the id itself for any id <= 222."""
         return x * torch.sigmoid(50 * (222.5 - x))
 
     def round_and_mask(self, x):
         return self.mask(self.round_ste(x))
 
-    def chebyshev_eval(self, x, coefficients):
-        """ Evaluate a Chebyshev polynomial expansion using PyTorch. """
-        # Normalize x to [-1, 1] range for Chebyshev evaluation
-        x_norm = x / 111 - 1
-        
-        # Initialize Chebyshev polynomials
-        T0 = torch.ones_like(x_norm, device=self.device)
-        T1 = x_norm.to(self.device)
-        
-        # Compute the polynomial expansion
-        result = coefficients[0] * T0 + coefficients[1] * T1
-        
-        for n in range(2, len(coefficients)):
-            T2 = 2 * x_norm * T1 - T0
-            result += coefficients[n] * T2
-            T0, T1 = T1, T2
-        
-        return result
-
-
 ######################
+
 
 def train_model(dataloader, model, criterion, optimizer, epochs, device):
     model.train()
@@ -270,9 +162,7 @@ def train_model(dataloader, model, criterion, optimizer, epochs, device):
             optimizer.zero_grad()
 
             y_pred = model(x)
-            # y_pred_transformed = transform_batch2(y_pred, data, device)
 
-            # loss = criterion(y_pred_transformed, m)
             loss = criterion(y_pred, y)
             loss.backward()
 
@@ -280,14 +170,14 @@ def train_model(dataloader, model, criterion, optimizer, epochs, device):
 
             total_loss += loss.item()
             num_batches += 1
-        
+
         epoch_loss = total_loss / num_batches
         epochs_bar.set_postfix_str(f"Loss = {epoch_loss:.4f}")
         loss_history.append(epoch_loss)
 
     plt.plot(loss_history)
-    # plt.savefig('loss_plot.png')
     plt.show()
+
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -297,17 +187,13 @@ if __name__ == "__main__":
 
     model = MenuGenerator().to(device)
     myLoss = MenuLoss(device).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
 
     print("Training...")
 
     x, _ = training_set[0]
 
-    y_pred = model(x.to(device))
-
-    #print(y_pred)
-
-    train_model(training_loader, model, myLoss, optimizer, 100, device)
+    train_model(training_loader, model, myLoss, optimizer, 500, device)
 
     y_pred = model(x.to(device))
 
--- Model/The_Model/poly.py
+++ Model/The_Model/poly.py
@@ -4,6 +4,7 @@ from numpy.polynomial.chebyshev import Chebyshev
 
 import matplotlib.pyplot as plt
 
+
 class RoundSTE(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input):
@@ -12,34 +13,39 @@ class RoundSTE(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_output):
         return grad_output
-    
+
+
 def round_ste(input):
     return RoundSTE.apply(input)
 
+
 def mask(x):
-    return x * torch.sigmoid(50 * (222.5 - x))
+    return x * torch.sigmoid(35 * (222.5 - x))
+
 
 def compose_them(x):
     return mask(round_ste(x))
 
+
 def chebyshev_eval(x, coefficients):
-    """ Evaluate a Chebyshev polynomial expansion using PyTorch. """
+    """Evaluate a Chebyshev polynomial expansion using PyTorch."""
     # Normalize x to [-1, 1] range for Chebyshev evaluation
     x_norm = x / 111 - 1
-    
+
     # Initialize Chebyshev polynomials
     T0 = torch.ones_like(x_norm)
     T1 = x_norm
-    
+
     # Compute the polynomial expansion
     result = coefficients[0] * T0 + coefficients[1] * T1
-    
+
     for n in range(2, len(coefficients)):
         T2 = 2 * x_norm * T1 - T0
         result += coefficients[n] * T2
         T0, T1 = T1, T2
-    
+
     return result
+
 
 print("Reading Data...")
 # Load data
@@ -47,12 +53,35 @@ data = read_foods_tensor()
 
 # Prepare x and y
 x = torch.tensor(range(len(data)))
-y = data[:, FP.VEGETARIAN.value]
+y = data[:, FP.VEGAN.value]
+
 
 def multiple_bell_curves(x):
     global y
 
-    return torch.sum(torch.stack([v * torch.exp(-((x - i * v) ** 2) / 0.00001) for i, v in enumerate(y)], dim=0), dim=0)
+    # dividing by 0.1 gives an error about 1e-5,
+    # dividing by 0.01 gives an error about 1e-44, so we may want that
+    return torch.sum(
+        torch.stack(
+            [v * torch.exp(-((x - i * v) ** 2) / 0.01) for i, v in enumerate(y)],
+            dim=0,
+        ),
+        dim=0,
+    )
+
+
+def multiple_bell_curves_mult(x):
+    global y
+
+    # here dividing by 0.01 gives an error about 6e-42 only for 0
+    return torch.sum(
+        torch.stack(
+            [v * torch.exp(-((x - i) ** 2) / 0.01) for i, v in enumerate(y)],
+            dim=0,
+        ),
+        dim=0,
+    )
+
 
 print("Fitting...")
 # Fit Chebyshev polynomial using NumPy
@@ -76,24 +105,31 @@ print("Fitting...")
 
 # Test with different input values
 # test = torch.tensor([0, 1, 10, 59.8, 100, 178.4, 200, 210, 220, 222, 222.5, 223, 224])
-test = torch.tensor([
-6, 12, 237.45, 0.83, 189, 76.29, 143, 251.67, 45, 112.56, 203,
-18.72, 134, 89.34, 211, 5.67, 167, 42.91, 226, 98, 155.23,
-67.45, 11, 242.78, 33, 176.54, 109, 54.12, 201.36, 87, 22.65,
-139, 71.89, 216, 39.47, 184, 62.13, 247, 105, 19.76, 230,
-53, 127.42, 94.56, 172, 7.31, 241, 115, 66.28, 198, 36.95, 0.00
-])
+# test = torch.tensor([
+# 6, 12, 237.45, 0.83, 189, 76.29, 143, 251.67, 45, 112.56, 203,
+# 18.72, 134, 89.34, 211, 5.67, 167, 42.91, 226, 98, 155.23,
+# 67.45, 11, 242.78, 33, 176.54, 109, 54.12, 201.36, 87, 22.65,
+# 139, 71.89, 216, 39.47, 184, 62.13, 247, 105, 19.76, 230,
+# 53, 127.42, 94.56, 172, 7.31, 241, 115, 66.28, 198, 36.95, 0.00
+# ])
 
+test = torch.range(0, 222.41, 0.7)
+# pred = multiple_bell_curves_mult(compose_them(test))
+pred = multiple_bell_curves(compose_them(test))
+gold = y[torch.round(test).long()]
 
-print("Evaluating...")
+for t, p, g in zip(test, pred, gold):
+    print(
+        f"{t:.2f} -> {p:.2f} vs {g:.2f}\t({abs(p - g)}{" !!!!!!!" if p != g else ''})"
+    )
+
+# print("Evaluating...")
 # test_fit = chebyshev_eval(compose_them(test), coefficients)
 
-# printable = [(test[i], test_fit[i]) for i in range(len(test))]
+# print(compose_them(torch.tensor(241.00)))
 
-print(compose_them(torch.tensor(241.00)))
-
-for x in test:
-    print(f"{x:.2f} -> {multiple_bell_curves(compose_them(x)):.2f}")
+# for x in test:
+#     print(f"{x:.2f} -> {multiple_bell_curves(compose_them(x)):.2f}")
 
 # print("Test input:", test)
 # # print the results line by line
