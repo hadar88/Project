bea27d70e476e55f421b7a4a865b809a1018e55e
hadar88 <hadargantz@gmail.com>
2025-03-12 16:50:55 +0200

    changes

--- App/Examples/Tutorial/main4.py
+++ App/Examples/Tutorial/main4.py
@@ -1,8 +1,20 @@
 from kivy.app import App
+from kivy.uix.widget import Widget
+
+class Touch(Widget):
+
+    def on_touch_down(self, touch):
+        print("Mouse Down", touch)
+    
+    def on_touch_move(self, touch):
+        print("Mouse Move", touch)
+
+    def on_touch_up(self, touch):
+        print("Mouse Up", touch)
 
 class Main4App(App):
     def build(self):
-        return
+        return Touch()
     
 
 if __name__ == "__main__":
