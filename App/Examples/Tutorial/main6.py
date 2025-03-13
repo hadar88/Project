2d3440b55858fec61624eee7f274970709d62b32
hadar88 <hadargantz@gmail.com>
2025-03-13 11:10:38 +0200

    changes

---
+++ App/Examples/Tutorial/main6.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectPropety
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+
+class Widgets(Widget):
+    pass
+
+class Main6App(App):
+    def build(self):
+        return Widgets()
+    
+if __name__ == "__main__":
+    Main6App().run()
