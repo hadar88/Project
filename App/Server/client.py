f8ac2371e49e1f44395924174696449f63253c60
Guychuk <moalemm@post.bgu.ac.il>
2025-04-09 13:06:46 +0300

    fixed the wgraph func in the server

--- App/Server/client.py
+++ App/Server/client.py
@@ -2,11 +2,9 @@ import requests
 
 SERVER_URL = "http://127.0.0.1:5000"
 
-data = {
-    "weights": [60, 61, 62, 70, 80],
-    "bmis": [12, 20.2, 20.5, 27, 30],
-    "times": ["2025-04-09", "2025-04-10", "2025-04-17", "2025-04-18", "2025-04-20"]
-}
+levels = [16, 18.5, 25, 30, 40]
+
+data = {"weights": [30, 30], "bmis": [15, 15], "times": ["2025-04-01", "2025-04-02"]}
 
 response = requests.get(f"{SERVER_URL}/wgraph", json=data)
 
--- App/Server/server.py
+++ App/Server/server.py
@@ -5,7 +5,7 @@ import requests
 import joblib
 import json
 import matplotlib.pyplot as plt
-from collections import defaultdict 
+from collections import defaultdict
 from utils import merge_ids_and_amounts
 from flask import Flask, jsonify, request, send_file
 from datetime import datetime, timedelta
@@ -18,19 +18,21 @@ class Server:
         self.app = Flask(__name__)
         self.setup_routes()
         self.start_wakeup_thread()
-        
+
     def setup_routes(self):
         @self.app.route("/")
         def home():
             return jsonify({"message": "Welcome to the NutriPlan API!"})
-        
+
         @self.app.route("/wakeup", methods=["GET"])
         def wakeup():
             return jsonify({"message": "Server is awake!"})
-        
+
         @self.app.route("/wgraph", methods=["GET"])
         def make_graph():
             data: dict = request.json
+
+            levels = [16, 18.5, 25, 30, 40]
 
             weights = data.get("weights", None)
             bmis = data.get("bmis", None)
@@ -38,19 +40,57 @@ class Server:
 
             if not weights or not bmis or not times:
                 return jsonify({"error": "Missing weights/bmis/times"}), 400
-            
+
             times = [datetime.strptime(date, "%Y-%m-%d") for date in times]
 
             plt.figure()
 
-            for i in range(len(bmis) - 1):
-                color = self.__bmi_decs_and_color(bmis[i + 1])[1]
-                plt.plot(times[i:i+2], weights[i:i+2], color=color)
+            if len(bmis) == 1:
+                # Plot one dot
+                plt.scatter(
+                    times[0], weights[0], color=self.__bmi_decs_and_color(bmis[0])[1]
+                )
 
-            plt.xticks([times[0], times[-1]], 
-                [times[0].strftime("%d-%m-%Y"), times[-1].strftime("%d-%m-%Y")], fontsize=15)
-            plt.yticks(fontsize=15) 
-            
+            for i in range(len(bmis) - 1):
+                # color = self.__bmi_decs_and_color(bmis[i + 1])[1]
+
+                b_m = (bmis[i + 1] - bmis[i]) / (
+                    times[i + 1] - times[i]
+                ).total_seconds()
+                w_m = (weights[i + 1] - weights[i]) / (
+                    times[i + 1] - times[i]
+                ).total_seconds()
+
+                t_0, w_0, b_0 = times[i], weights[i], bmis[i]
+                t_1, w_1, b_1 = times[i + 1], weights[i + 1], bmis[i + 1]
+
+                for level in levels:
+                    color = self.__bmi_decs_and_color(b_0)[1]
+
+                    # We should split the line into two lines if the BMI level is crossed
+                    if b_0 < level < b_1:
+                        # Calculate the intersection point
+                        b_ = level
+                        t_delta = (
+                            b_ - b_0
+                        ) / b_m  # This gives the time difference in seconds
+                        t_ = t_0 + timedelta(
+                            seconds=t_delta
+                        )  # Add the time difference to t_0
+                        w_ = w_0 + w_m * (t_ - t_0).total_seconds()
+                        plt.plot([t_0, t_], [w_0, w_], color=color)
+
+                        t_0, w_0, b_0 = t_, w_, b_
+                    else:
+                        plt.plot([t_0, t_1], [w_0, w_1], color=color)
+
+            plt.xticks(
+                [times[0], times[-1]],
+                [times[0].strftime("%d-%m-%Y"), times[-1].strftime("%d-%m-%Y")],
+                fontsize=15,
+            )
+            plt.yticks(fontsize=15)
+
             img_buffer = io.BytesIO()
 
             plt.savefig(img_buffer, format="png")
@@ -82,7 +122,7 @@ class Server:
             merged_pred = merge_ids_and_amounts(pred_id, pred_amount)
 
             return jsonify({"output": merged_pred.tolist()})
-        
+
     def __bmi_decs_and_color(self, bmi_val):
         if bmi_val < 16:
             return ("Severely underweight", (0, 0, 1, 1))
@@ -96,7 +136,7 @@ class Server:
             return ("Obese", (1, 0.5, 0, 1))
         else:
             return ("Extremely obese", (1, 0, 0, 1))
-        
+
     def start_wakeup_thread(self):
         def send_wakeup_request():
             while True:
@@ -111,54 +151,3 @@ class Server:
 
     def run(self, host="0.0.0.0", port=5000):
         self.app.run(host=host, port=port, debug=False)
-
-
-"""
-        @self.app.route("/wgraph", methods=["GET"])
-        def make_graph():
-            data: dict = request.json
-
-            levels = [16, 18.5, 25, 30, 40]
-
-            weights = data.get("weights", None)
-            bmis = data.get("bmis", None)
-            times = data.get("times", None)
-
-            if not weights or not bmis or not times:
-                return jsonify({"error": "Missing weights/bmis/times"}), 400
-            
-            times = [datetime.strptime(date, "%Y-%m-%d") for date in times]
-
-            plt.figure()
-
-            for i in range(len(bmis) - 1):
-                # color = self.__bmi_decs_and_color(bmis[i + 1])[1]
-
-                m = (bmis[i + 1] - bmis[i]) / (times[i + 1] - times[i]).days
-                x0, y0 = times[i], weights[i]
-
-                for level in levels:
-                    
-
-                if bmis[i] < 16 and bmis[i + 1] < 16:
-                    color = (0, 0, 1, 1)
-                    y1 = 16
-                    x1 = (y1 - y0 - m * x0) / m
-                    plt.plot([x0, x1], [y0, y1], color=color)
-                    x0, y0 = x1, y1
-
-                plt.plot(times[i:i+2], weights[i:i+2], color=color)
-
-            plt.xticks([times[0], times[-1]], 
-                [times[0].strftime("%d-%m-%Y"), times[-1].strftime("%d-%m-%Y")], fontsize=15)
-            plt.yticks(fontsize=15) 
-            
-            img_buffer = io.BytesIO()
-
-            plt.savefig(img_buffer, format="png")
-            plt.close()
-
-            img_buffer.seek(0)
-
-            return send_file(img_buffer, mimetype="image/png")
-"""
